ab909846da561b15fa475cf61f5743f63cbabb7d
thiago balbino <thiagobalbino91@gmail.com>
2021-11-10 20:15:25 -0300

    initial commit

---
+++ cliente/src/test/java/br/com/desafio/builder/cliente/ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.builder.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
